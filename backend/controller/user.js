a54f70a5758eeeb3e87bb7693b7f7dc92bd68d67
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-05-16 23:16:57 +0530

    Your user's password is at risk!!!!

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,54 +1,73 @@
 const User = require("../models/user");
+const bycript = require("bcrypt");
 
 exports.signUpUser = (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-
-  User.create({
-    name: name,
-    email: email,
-    password: password,
-  })
-    .then((result) => {
-      res.status(201).json({ message: result });
+  const saltRounds = 10;
+  bycript.hash(password, saltRounds, (err, hash) => {
+    User.create({
+      name: name,
+      email: email,
+      password: hash
     })
-    .catch((err) => {
-      if (
-        err.name === "SequelizeUniqueConstraintError" &&
-        err.errors[0].type === "unique violation" &&
-        err.errors[0].path === "email"
-      ) {
-        res.status(400).json({
-          message: "User Already Exists. Please Login.",
-        });
-      }
-      res.json({ message: err });
-    });
+      .then((result) => {
+        res.status(201).json({ message: result });
+      })
+      .catch((err) => {
+        if (
+          err.name === "SequelizeUniqueConstraintError" &&
+          err.errors[0].type === "unique violation" &&
+          err.errors[0].path === "email"
+        ) {
+          res.status(400).json({
+            message: "User Already Exists. Please Login.",
+          });
+        }
+        res.json({ message: err });
+      });
+  });
 };
 
-exports.loginUser = (req,res,next) =>{
+exports.loginUser = async (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({
-        where :{
-            email : email,
+    try {
+        // Find the user by email
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found with provided email."
+            });
         }
-    })
-    .then((result) =>{
-        //res.json(result)
-         if(result.password === password)
-         {
-            res.status(200).json({message:"Login Successfull"})
-         }else if(result.password !== password)
-         {
-            res.status(401).json({message:"User not authorized"})
-         }else{
-            res.status(400).json({message:"User not found"})
-         }
-    })
-    .catch((err) =>{
-        res.status(400).json({message:"User not found"})
-    })
-}
+         bycript.compare(password, user.password , (err,result) =>{
+            if(result)
+            {
+                res.status(200).json({
+                    message: "User authenticated successfully.",
+                    user: user
+                });
+                
+            }else{
+                res.status(401).json({
+                    message: "Invalid password."
+                });
+            }
+         });
+
+        
+    } catch (error) {
+        res.status(500).json({
+            message: "An error occurred while searching for the user.",
+            error: error
+        });
+    }
+};
+
