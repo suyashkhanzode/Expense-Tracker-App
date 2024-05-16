b03b9ddae0a0cdc77fa66f6dbbe66a395f09f9bd
“suyashkhanzode” <suyashkhanzode123@gmail.com>
2024-05-16 22:20:15 +0530

    Make the backend api of the login

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -26,3 +26,29 @@ exports.signUpUser = (req, res, next) => {
       res.json({ message: err });
     });
 };
+
+exports.loginUser = (req,res,next) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({
+        where :{
+            email : email,
+        }
+    })
+    .then((result) =>{
+        //res.json(result)
+         if(result.password === password)
+         {
+            res.status(200).json({message:"Login Successfull"})
+         }else if(result.password !== password)
+         {
+            res.status(401).json({message:"User not authorized"})
+         }else{
+            res.status(400).json({message:"User not found"})
+         }
+    })
+    .catch((err) =>{
+        res.status(400).json({message:"User not found"})
+    })
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post('/sign-up',userController.signUpUser)
 
+router.post('/login',userController.loginUser)
+
 module.exports = router
